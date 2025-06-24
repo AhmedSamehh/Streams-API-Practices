51a7dd65ee209f910326ade511fabc70563d1936
asameh <a7medse7a10@gmail.com>
2025-06-24 20:06:13 +0300

    B_SumOfUniqueNumbers

--- src/test/java/com/github/streams/practice/numbers/problems/B_SumOfUniqueNumbers.java
+++ src/test/java/com/github/streams/practice/numbers/problems/B_SumOfUniqueNumbers.java
@@ -15,11 +15,11 @@ import org.junit.jupiter.api.Test;
 class B_SumOfUniqueNumbers {
 
   @Test
-  @Disabled
+//  @Disabled
   void testSumOfUniqueDigitsTest() {
     final var input = List.of(5, 6, 7, 8, 5, 5, 8, 8, 7);
     var mySolution = NumbersProblemSolution.sumOfUniqueNumbers(input);
-    var yourSolution = List.of();
+    var yourSolution = input.stream().distinct().mapToInt(value -> value.intValue()).sum();
 
     Assertions.assertEquals(mySolution, yourSolution);
   }
