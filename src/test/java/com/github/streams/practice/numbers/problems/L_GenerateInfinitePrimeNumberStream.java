31141ea5e4a1b284dd2fbcfd29bc49286b08ab8a
asameh <a7medse7a10@gmail.com>
2025-07-05 18:29:06 +0300

    L_GenerateInfinitePrimeNumberStream

--- src/test/java/com/github/streams/practice/numbers/problems/L_GenerateInfinitePrimeNumberStream.java
+++ src/test/java/com/github/streams/practice/numbers/problems/L_GenerateInfinitePrimeNumberStream.java
@@ -1,6 +1,8 @@
 package com.github.streams.practice.numbers.problems;
 
 import com.github.streams.practice.numbers.NumbersProblemSolution;
+
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
@@ -9,10 +11,10 @@ import org.junit.jupiter.api.Test;
 class L_GenerateInfinitePrimeNumberStream {
 
   @Test
-  @Disabled
+//  @Disabled
   void intStreamOfPrimeNumber() {
     final var mySolution = NumbersProblemSolution.generateIntStreamOfPrimeNumberStream(5);
-    final var yourSolution = IntStream.range(0, 10);
+    final var yourSolution = IntStream.range(0, 12).filter(x-> x==2 || x==3 || (x>1 && x%2!=0 && x%3!=0)).boxed().collect(Collectors.toList());
 
     Assertions.assertEquals(mySolution, yourSolution);
   }
